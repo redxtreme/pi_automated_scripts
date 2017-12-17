8c599e3286b2eccc81669b6ca1fd31788242284f
redxtreme <zahopper@mtu.edu>
2017-12-16 19:16:44 -0600

    Ticket info is gathered

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pi_automated_scripts
+These are some automation scripts I'm using on my pi.
+
+## References
+
+### Node calling python
+https://stackoverflow.com/questions/23450534/how-to-call-python-function-from-nodejs#23452742
--- stubbs_concert_tickets/concert_scraper.py
+++ stubbs_concert_tickets/concert_scraper.py
@@ -4,12 +4,16 @@
 import bs4, requests, pprint, re
 
 URL = 'https://www.stubbsaustin.com/concert-listings'
-SHOW_DETAILS_CLASS_NAME = 'frontgateFeedSummary'
-CONCERT_TITLE_CLASS_NAME = 'contentTitle'
+CONCERT_CONTAINER_CLASS = 'frontgateFeedSummary'
+CONCERT_TITLE_CLASS = 'contentTitle'
+CONCERT_WEEKDAY_CLASS = 'eventWeekday'
+CONCERT_MONTH_CLASS = 'eventMonth'
+CONVERT_DAY_CLASS = 'eventDay'
+CONCERT_DETAILS_CLASS = 'frontgateFeedShowDetails'
 
 #Get the shows on the website
 def getConcerts():
-    titles = []
+    titles = {}
     res = requests.get(URL)
     res.raise_for_status()
 
@@ -17,11 +21,16 @@ def getConcerts():
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
     #find all of the concert container elements
-    concert_containers = soup.find_all(class_=SHOW_DETAILS_CLASS_NAME)
+    concert_containers = soup.find_all(class_=CONCERT_CONTAINER_CLASS)
 
     #loop through all of the show containers and grab the titles
     for concert in concert_containers:
-        titles.append(concert.find(class_=CONCERT_TITLE_CLASS_NAME).text)
+        new_concert = {}
+        new_concert['title'] = concert.find(class_=CONCERT_TITLE_CLASS).text
+        new_concert['weekday'] = concert.find(class_=CONCERT_WEEKDAY_CLASS).text
+        new_concert['month'] = concert.find(class_=CONCERT_MONTH_CLASS).text
+        new_concert['details'] = concert.find(class_=CONCERT_DETAILS_CLASS).text
+        titles[new_concert['title']] = new_concert
 
     return titles
 
